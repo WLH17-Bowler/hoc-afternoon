390c8e0dc4e740b3560daec6d8f619ca8b553f11
Josh Bowler <joshbowler46@gmail.com>
2020-04-06 16:18:23 -0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+import ExchangedCurrency from './Components/CurrencyConverter'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <h2> Higher Order Components </h2>
+        <ExchangedCurrency  />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
